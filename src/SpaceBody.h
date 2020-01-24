3c9b97b3e557680920f525bde437216a53be00cf
Pavel <pavelefainov@gmail.com>
2020-01-24 11:29:02 +0300

    SpaceBody added

---
+++ src/SpaceBody.h
@@ -0,0 +1,16 @@
+//
+// Created by lizzka239 on 1/24/2020.
+//
+
+#ifndef ASTEROIDS_SPACEBODY_H
+#define ASTEROIDS_SPACEBODY_H
+
+
+class SpaceBody {
+public:
+protected:
+private:
+};
+
+
+#endif //ASTEROIDS_SPACEBODY_H
