bad638a26f7773b49d51d2294bf59b4f07a3fe95
PavelEfarinov <pavelefainov@gmail.com>
2020-01-24 12:08:55 +0300

    Added CommandReader

---
+++ src/CommandReader.cpp
@@ -0,0 +1,35 @@
+//
+// Created by fools on 1/24/2020.
+//
+
+#include "CommandReader.h"
+
+CommandReader::CommandReader() {
+    mKeys = new std::vector<bool>(sf::Keyboard::KeyCount, false);
+    mLastReleased = sf::Keyboard::Key::Unknown;
+}
+
+void CommandReader::updateKeyboard(sf::Event e) {
+    mLastReleased = sf::Keyboard::KeyCount;
+    switch (e.type) {
+        case sf::Event::KeyReleased:
+            mLastReleased = e.key.code;
+            (*mKeys)[e.key.code] = false;
+            break;
+
+        case sf::Event::KeyPressed:
+            (*mKeys)[e.key.code] = true;
+            break;
+
+        default:
+            break;
+    }
+}
+
+bool CommandReader::isKeyDown(sf::Keyboard::Key key) const {
+    return (*mKeys)[key];
+}
+
+bool CommandReader::keyReleased(sf::Keyboard::Key key) const {
+    return mLastReleased == key;
+}
