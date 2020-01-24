9ae8d9de1f310f4aaf2dccf054c79a7fcd41c13a
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2020-01-24 15:12:49 +0300

    added Ship

---
+++ src/Ship.h
@@ -0,0 +1,14 @@
+//
+// Created by lizzka239 on 1/24/2020.
+//
+
+#ifndef ASTEROIDS_SHIP_H
+#define ASTEROIDS_SHIP_H
+
+
+class Ship {
+
+};
+
+
+#endif //ASTEROIDS_SHIP_H
--- src/SpaceBody.cpp
+++ src/SpaceBody.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "SpaceBody.h"
+
+SpaceBody::SpaceBody(sf::Vector2f pos, sf::Vector2f speed, sf::Vector2f acceleration, sf::Sprite& sprite)  {
+    mPosition = pos;
+    mSpeed = speed;
+    mAcceleration = acceleration;
+    mProjection = sprite;
+}
+
+void SpaceBody::move(sf::Time time) {
+    mPosition = mPosition +
+}
